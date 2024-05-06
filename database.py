7a77d16fd13fea29c1f131b3df71e1865a61cea9
tryraisins <tryraisins@gmail.com>
2024-05-06 20:30:37 +0100

    fix issue with database naming

--- database.py
+++ database.py
@@ -3,4 +3,4 @@ from schemas import Patients, Doctors, Appointment
 
 patients_data: Dict[int, Patients] = {}
 doctors_data: Dict[int, Doctors] = {}
-appointments_data: Dict[int, Appointment] = {}
+appointment_data: Dict[int, Appointment] = {}
