6351cac449ced88d276cf3f69018565daaed7575
tryraisins <tryraisins@gmail.com>
2024-05-06 21:50:33 +0100

    removed type definition in databse file

--- database.py
+++ database.py
@@ -1,6 +1,3 @@
-from typing import Dict
-from schemas import Patients, Doctors, Appointment
-
-patients_data: Dict[int, Patients] = {}
-doctors_data: Dict[int, Doctors] = {}
-appointment_data: Dict[int, Appointment] = {}
+patients_data= {}
+doctors_data = {}
+appointment_data = {}
