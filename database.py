865e1a8e6b0c7c8f88fa439968270f7d7c966a9a
tryraisins <tryraisins@gmail.com>
2024-05-06 20:28:02 +0100

    tried to fix issues with succesful responses

--- database.py
+++ database.py
@@ -1,3 +1,6 @@
-patients_data = {}
-doctors_data = {}
-appointment_data = {}
+from typing import Dict
+from schemas import Patients, Doctors, Appointment
+
+patients_data: Dict[int, Patient] = {}
+doctors_data: Dict[int, Doctor] = {}
+appointments_data: Dict[int, Appointment] = {}
